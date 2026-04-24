bffcbd7e22267463fbc388f30bafe7e141f9e3eb
666arevin <arevin6019@gmail.com>
2026-04-24 21:03:28 +0300

    теперь кнопка войти реально отправляет данные на сервер

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from . import app
 from flask import render_template
+from flask import redirect, url_for, request
 
 def print_hello():
     print("Hello, word!")
@@ -9,3 +10,10 @@ def print_hello():
 def index():
     return render_template("authorization.html")
 
+@app.route('/api/fetch', methods=['POST'])
+def authorization():
+    login = request.form.get('login')
+    password = request.form.get('password')
+    print(f"Login - {login}, password - {password}.")
+
+    return redirect(url_for('index'))
