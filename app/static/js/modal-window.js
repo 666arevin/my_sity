64d09f154f739e30d58933913747dd72255fad26
666arevin <arevin6019@gmail.com>
2026-04-24 13:26:08 +0300

    стилизация окна авторизации

--- app/static/js/modal-window.js
+++ app/static/js/modal-window.js
@@ -1,16 +1,37 @@
 
+const Buttons = document.querySelector('.list-bar');
+const ModalWindow = document.querySelector('.modal-overlay-blur');
+
+function closemodal(event){
+    if (event.target === ModalWindow) {
+        ModalWindow.classList.remove('visible');
+        console.info('close');
+    }
+}
+
+
+function OpenModalWindow(event) {
+    event.stopPropagation();
+
+    if (event.target.id === 'authorization-button') {
+        ModalWindow.classList.add('visible');
+        ModalWindow.addEventListener('click', closemodal, {once: true})
+    }
+}
+
+Buttons.addEventListener('click', OpenModalWindow);
+
 const ButtonContainer2 = document.querySelector('.modal-choise');
-const BackGroundToggle = document.querySelector('.toggle-bg')
+const BackGroundToggle = document.querySelector('.toggle-bg');
 
 function AnimationBG(event) {
 
     if (event.target.id === 'button-log') {
-        BackGroundToggle.classList.remove('active')
+        ModalWindow.classList.remove('register')
     }
     
     if (event.target.id === 'button-reg') {
-
-        BackGroundToggle.classList.add('active')
+        ModalWindow.classList.add('register')
     }
 
 }
