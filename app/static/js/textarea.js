209e170606bf6e89254a1070d2d65ad15b0d22bb
666arevin <arevin6019@gmail.com>
2026-04-26 14:49:01 +0300

    сделал чтобы окно для ввоода росло сначало в ширину

--- app/static/js/textarea.js
+++ app/static/js/textarea.js
@@ -1,20 +1,28 @@
 const textarea = document.querySelector('#user-prompt');
-const ChatBlock  = document.querySelector('.chat-block ')
+const ChatBlock = document.querySelector('.chat-block ')
+// блок к котром лежит textarea
+const MainBlock = document.querySelector('.search-string')
 
 function adjustHeight(el, ChatBlock) {
-    el.style.height = 'auto';
-    el.style.height = el.scrollHeight + 'px';
-
+    if (el.scrollWidth >= 615) {
+        el.style.minWidth = '615px'; 
+        el.style.wordBreak = 'break-all';
+        el.style.height = 'auto';
+        el.style.height = el.scrollHeight + 'px';
+        console.log('высота - ' + el.scrollHeight)
+    } else {
+        el.style.width = 'auto';
+        el.style.width = el.scrollWidth + 'px';;
+        console.log('ширина - ' + el.scrollWidth)
+    };
 
     if (el.scrollHeight > 24) {
         ChatBlock.classList.add('big-area');
 
-    }
-
-    if (el.scrollHeight < 40) {
+    } else if (el.scrollHeight < 40) {
         ChatBlock.classList.remove('big-area');
-    }
-    
+    };
+
 };
 
 textarea.addEventListener('input', () => {
