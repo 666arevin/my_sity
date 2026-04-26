41698c7113b56b25a93119a1d91e5125c722fb53
666arevin <arevin6019@gmail.com>
2026-04-26 15:58:59 +0300

    переработка поля для ввода, теперь это тег div. Исправлен баг при вводе. Теперь поле для ввода сначала расширяется в длинну а потом растет вниз, что дает больш епространства для пользователя

--- app/static/js/textarea.js
+++ app/static/js/textarea.js
@@ -2,18 +2,15 @@ const textarea = document.querySelector('#user-prompt');
 const ChatBlock  = document.querySelector('.chat-block ')
 
 function adjustHeight(el, ChatBlock) {
-    el.style.height = 'auto';
-    el.style.height = el.scrollHeight + 'px';
-
 
     if (el.scrollHeight > 24) {
         ChatBlock.classList.add('big-area');
 
-    }
+    };
 
     if (el.scrollHeight < 40) {
         ChatBlock.classList.remove('big-area');
-    }
+    };
     
 };
 
