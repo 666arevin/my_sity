8f37f36400d095c4335f356da9955c66c99e7d06
666arevin <arevin6019@gmial.com>
2026-09-02 11:55:59 +0300

    Удалил странныую пустую директорию. Добавил файл с парсером. Убрал ключ Openrouter пиз файла и переместил в .env.

---
+++ app/static/js/parser.js
@@ -0,0 +1,46 @@
+// перменная в которой накапливаются не // обработанные чанк 
+let MarkdownBuffer = "";
+
+//паттерны распознования однострочный элементов
+const SLPattern = ["#", "**", "*", "`", "[", "---"];
+// паттерны для многостросный символов, разрыыаются /n/n
+const MLPPattwrn= [">", "-", "1.", "~~"];
+// если равен 3, значит началась таблица
+let TablrDetector = 0;
+// если равен 1 значит ждем /n и очищаем
+let SLDetector = 0
+// есоли равен 1, знкчит жжем /n/n или парного тега и очищаем
+let MLPDetector = 0
+
+// значение от 0 до 2, количество перенсов строки в чанке
+// Lone-Break Detector
+let LBDetector = 0
+
+function SmartBuffer (ChunkContent) {
+
+  // проходимся по символам в котенте чанка
+  for (let i in ChunkContent) {
+    // есои нашли символ, изменяем detector для однострочных
+    if (i in SLPattern) {
+      SLDetector = 1;
+    };
+    if (i == "\n") {
+      LBDetector += 1;
+    };
+  };
+  
+//----------
+// тут парсим текст 
+  MarkdownBuffer += ChunkContent
+  HTMLText = parse(MarkdownBuffer)
+// ----------
+  
+// после проверок мы очищаем или не очищаем буффер
+// если однострочный режим и был \n чистим буффер
+  if (SLDetector == 1 && LBDetector == 1) {
+    // очищаем буффер
+    
+    SLDetector = 0
+    LBDetector = 0
+  };
+};
